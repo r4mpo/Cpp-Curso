33403cf2e370b6d80ab26a88d83951c8d88760f8
r4mpo <erick1souza1ago04@gmail.com>
2025-01-25 12:06:31 -0300

    Primeiro Commit - Conhecendo a linguagem

---
+++ primeiros-comandos/01-saida-de-dados-sem-namespace-std.cpp
@@ -0,0 +1,11 @@
+// importando biblioteca de entrada e saída de dados
+#include<iostream>
+
+//  int main() = função principal do nosso sistema
+int main()
+{
+    // antes de usar o namespace std:
+    std::cout << "****************************************************" << std::endl;
+    std::cout << "*Esta é a primeira saída de dados que temos em C++!*" << std::endl;
+    std::cout << "****************************************************" << std::endl;
+}
---
+++ primeiros-comandos/02-saida-de-dados-com-namespace-std.cpp
@@ -0,0 +1,14 @@
+// importando biblioteca de entrada e saída de dados
+#include<iostream>
+// importando todas as funções do std
+using namespace std;
+
+//  int main() = função principal do nosso sistema
+int main()
+{
+    // depois de usar o namespace std:
+    cout << "******************************************************************************" << endl;
+    cout << "*Esta é a primeira saída de dados que temos em C++, usando o namespace std!!!*" << endl;
+    cout << "******************************************************************************" << endl;
+    // note que não é mais necessário usar "std::"
+}
