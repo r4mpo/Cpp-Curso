d6f653a6e780c465defc60fefd4595a145e0cbc4
r4mpo <erick1souza1ago04@gmail.com>
2025-02-03 20:05:33 -0300

    Finalização do Jogo da Forca

--- jogo-forca/index.cpp
+++ jogo-forca/index.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "Programacao";
+map<char, bool> chutou_letra;
+vector<char> chutes_errados;
 
 bool contem_letra(char letra)
 {
@@ -29,24 +34,99 @@ bool contem_letra(char letra)
     return contem_letra;
 }
 
+bool nao_acertou()
+{
+    for (char letra : PALAVRA_SECRETA)
+    {
+        if (!chutou_letra[letra])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool nao_enforcou()
+{
+    return chutes_errados.size() < 5;
+}
+
+void imprime_cabecalho()
+{
+    cout << "***************************************************" << endl;
+    cout << "** SEJA BEM-VINDO AO JOGO DA FORCA FEITO COM C++ **" << endl;
+    cout << "***************************************************" << endl;
+}
+
+void imprime_chutes_errados()
+{
+    cout << "Chutes errados: ";
+
+    for (char letra : chutes_errados)
+    {
+        cout << letra << " ";
+    }
+}
+
+void imprime_palavra_secreta()
+{
+    cout << endl
+         << "Palavra secreta: ";
+
+    for (char letra : PALAVRA_SECRETA)
+    {
+        char letra_individual;
+
+        if (chutou_letra[letra])
+        {
+            letra_individual = letra;
+        }
+        else
+        {
+            letra_individual = '_';
+        }
+
+        cout << letra_individual << " ";
+    }
+}
+
 int main()
 {
-    bool nao_acertou = true;
-    bool nao_enforcou = true;
+    imprime_cabecalho();
 
-    while (nao_acertou && nao_enforcou)
+    cout << endl;
+
+    while (nao_acertou() && nao_enforcou())
     {
+
+        imprime_chutes_errados();
+
+        imprime_palavra_secreta();
+
+        cout << endl
+             << endl;
+        ;
+
         char chute;
+
+        cout << "Digite seu chute: ";
+
         cin >> chute;
+
+        chutou_letra[chute] = true;
 
         if (contem_letra(chute))
         {
-            cout << "Letra esta presente na palavra";
+            cout << "O Chute - " << chute << " - foi contabilizado como acerto." << endl;
         }
 
         else
         {
-            cout << "Letra nao esta presente na palavra";
+            cout << "O Chute - " << chute << " - foi contabilizado como erro." << endl;
+            chutes_errados.push_back(chute);
         }
+
+        cout << endl;
     }
 }
